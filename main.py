3d6079c31279c34787bf33e62ee5571a848f8937
Carolyne Soares <157962542+CarolyneSoares@users.noreply.github.com>
2024-03-19 21:34:47 -0300

    main.py

--- main.py
+++ main.py
@@ -12,10 +12,9 @@ def deal_card():
 def calculate_score(cards):
   """Take a list of cards and return the score calculated from the cards"""
 
-  
   if sum(cards) == 21 and len(cards) == 2:
     return 0
- 
+  
   if 11 in cards and sum(cards) > 21:
     cards.remove(11)
     cards.append(1)
@@ -26,6 +25,8 @@ def compare(user_score, computer_score):
   
   if user_score > 21 and computer_score > 21:
     return "You went over. You lose 😤"
+
+
   if user_score == computer_score:
     return "Draw 🙃"
   elif computer_score == 0:
@@ -33,7 +34,7 @@ def compare(user_score, computer_score):
   elif user_score == 0:
     return "Win with a Blackjack 😎"
   elif user_score > 21:
-    return "Youy went over. You lose 😭"
+    return "You went over. You lose 😭"
   elif computer_score > 21:
     return "Opponent went over. You win 😁"
   elif user_score > computer_score:
@@ -45,7 +46,7 @@ def play_game():
 
   print(logo)
 
-  
+ 
   user_cards = []
   computer_cards = []
   is_game_over = False
@@ -54,9 +55,11 @@ def play_game():
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
- 
+  
+
   while not is_game_over:
-   
+    
+    user_score = calculate_score(user_cards)
     computer_score = calculate_score(computer_cards)
     print(f"   Your cards: {user_cards}, current score: {user_score}")
     print(f"   Computer's first card: {computer_cards[0]}")
@@ -64,6 +67,7 @@ def play_game():
     if user_score == 0 or computer_score == 0 or user_score > 21:
       is_game_over = True
     else:
+      
       user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
       if user_should_deal == "y":
         user_cards.append(deal_card())
@@ -83,3 +87,4 @@ def play_game():
 while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
   clear()
   play_game()
+
